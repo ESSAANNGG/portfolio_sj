e2f17b6893f61c2d9212c3b019f624578e1ad1f2
LSJ97 <tkdwnsdk11@gmail.com>
2022-02-21 23:40:54 +0900

    Initial commit

---
+++ src/main/java/com/sjport/portfolio_sj/PortfolioSjApplication.java
@@ -0,0 +1,13 @@
+package com.sjport.portfolio_sj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioSjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioSjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sjport/portfolio_sj/PortfolioSjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjport.portfolio_sj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

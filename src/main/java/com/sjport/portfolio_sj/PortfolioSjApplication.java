5b6ddfc016ec9d374bcaa77cba0bc47b3b083186
LSJ97 <tkdwnsdk11@gmail.com>
2022-02-23 23:25:55 +0900

    컨트롤러 생성

--- src/main/java/com/sjport/portfolio_sj/PortfolioSjApplication.java
+++ src/main/java/com/sjport/portfolio_sj/PortfolioSjApplication.java
@@ -2,8 +2,9 @@ package com.sjport.portfolio_sj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PortfolioSjApplication {
 
     public static void main(String[] args) {

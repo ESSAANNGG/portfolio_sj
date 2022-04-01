80bf426bbacd59cd73881322b77d345fc7ef8d97
ESSAANNGG <tkdwnsdk11@gmail.com>
2022-04-01 23:57:05 +0900

    DTO추가

---
+++ src/main/java/com/sjport/portfolio_sj/dto/BoardDto.java
@@ -0,0 +1,11 @@
+package com.sjport.portfolio_sj.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BoardDto {
+    private Long gno;
+    private String title;
+    private String content;
+    private String writer;
+}
